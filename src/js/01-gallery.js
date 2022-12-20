fa809bfc3ef6c639f3bf4876390b752eead326f7
Lukyianenko <110479686+Lukyianenko@users.noreply.github.com>
2022-12-20 21:07:24 +0200

    add files

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,9 +1,5 @@
 // Add imports above this line
-
-
 import { galleryItems } from './gallery-items';
-
-import SimpleLightbox from "simplelightbox";
 // Change code below this line
 
-console.log(SimpleLightbox);
+console.log(galleryItems);
