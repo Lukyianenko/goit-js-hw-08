61d8eccafb824accf04f2ffca4448e56c96ce0bd
Lukyianenko <110479686+Lukyianenko@users.noreply.github.com>
2022-12-21 20:16:18 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+    import Player from "@vimeo/player";
+
+    const iframe = document.querySelector('#vimeo-player');
+    const player = new Player(iframe);
+   
+    player.on('timeupdate', function(data) {
+    const timeStop = data.seconds;
+    localStorage.setItem("videoplayer-current-time", timeStop);
+    });
+
+    const timeStart = localStorage.getItem("videoplayer-current-time");
+    player.setCurrentTime(timeStart);
 
