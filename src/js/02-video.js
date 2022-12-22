86d71af949e7b21b4f00a6eb2083fc956685bc1d
Lukyianenko <110479686+Lukyianenko@users.noreply.github.com>
2022-12-22 21:26:21 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,11 +4,19 @@
     const iframe = document.querySelector('#vimeo-player');
     const player = new Player(iframe);
    
+    onReloadPage();
+    
     player.on('timeupdate', throttle(function(data) {
     const timeStop = data.seconds;
     localStorage.setItem("videoplayer-current-time", timeStop);
-    }));
+    }, 500));
 
-    const timeStart = localStorage.getItem("videoplayer-current-time");
-    player.setCurrentTime(timeStart);
+
+    function onReloadPage() {
+        const timeStart = localStorage.getItem("videoplayer-current-time");
+        if (timeStart) {
+            player.setCurrentTime(timeStart);
+        }  
+    }
+    
 
